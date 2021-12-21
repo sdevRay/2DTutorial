5067616dfff7452c989c214cedea68845d61f147
sdevRay <ray.hancock.c@gmail.com>
2021-12-20 23:58:25 -0500

    Specifying the position of an image on the screen

--- 2DTutorial.Game/Game1.cs
+++ 2DTutorial.Game/Game1.cs
@@ -4,6 +4,15 @@ using Microsoft.Xna.Framework.Input;
 
 namespace _2DTutorial
 {
+    public struct PlayerData
+    {
+        public Vector2 Position;
+        public bool IsAlive;
+        public Color Color;
+        public float Angle;
+        public float Power;
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
@@ -12,7 +21,25 @@ namespace _2DTutorial
 
         private Texture2D _backgroundTexture;
         private Texture2D _foregroundTexture;
+        private Texture2D _carriageTexture;
+        private Texture2D _cannonTexture;
+        
+        private int _numPlayers = 4;
+        private PlayerData[] _players;
 
+        private Color[] _playerColors = new Color[10]
+        {
+             Color.Red,
+             Color.Green,
+             Color.Blue,
+             Color.Purple,
+             Color.Orange,
+             Color.Indigo,
+             Color.Yellow,
+             Color.SaddleBrown,
+             Color.Tomato,
+             Color.Turquoise
+        };
 
         private int _screenWidth;
         private int _screenHeight;
@@ -41,11 +68,17 @@ namespace _2DTutorial
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _device = _graphics.GraphicsDevice;
 
-            _screenWidth = _device.PresentationParameters.BackBufferWidth; 
+            _screenWidth = _device.PresentationParameters.BackBufferWidth;
             _screenHeight = _device.PresentationParameters.BackBufferHeight;
+
+            SetUpPlayers();
 
             _backgroundTexture = Content.Load<Texture2D>("background");
             _foregroundTexture = Content.Load<Texture2D>("foreground");
+
+            _carriageTexture = Content.Load<Texture2D>("carriage");
+            _cannonTexture = Content.Load<Texture2D>("cannon");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -63,16 +96,47 @@ namespace _2DTutorial
 
             _spriteBatch.Begin();
             DrawScenery();
-            _spriteBatch.End(); 
+            DrawPlayers();
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
-    
+
         private void DrawScenery()
-		{
+        {
             var screenRect = new Rectangle(0, 0, _screenWidth, _screenHeight);
             _spriteBatch.Draw(_backgroundTexture, screenRect, Color.White);
             _spriteBatch.Draw(_foregroundTexture, screenRect, Color.White);
-		}
+        }
+
+        private void DrawPlayers()
+        {
+            for(int i = 0; i < _players.Length; i++)
+            {
+                if (_players[i].IsAlive)
+                {
+                    _spriteBatch.Draw(_carriageTexture, _players[i].Position, Color.White);
+                }
+            }
+        }
+        private void SetUpPlayers()
+        {
+            _players = new PlayerData[_numPlayers];
+            for (int i = 0; i < _numPlayers; i++)
+            {
+                _players[i] = new PlayerData()
+                {
+                    IsAlive = true,
+                    Color = _playerColors[i],
+                    Angle = MathHelper.ToRadians(90),
+                    Power = 100
+                };
+            }
+
+            _players[0].Position = new Vector2(100, 193);
+            _players[1].Position = new Vector2(200, 212);
+            _players[2].Position = new Vector2(300, 361);
+            _players[3].Position = new Vector2(400, 164);
+        }
     }
 }
